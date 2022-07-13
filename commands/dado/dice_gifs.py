d491219c1cb0f8ddff806bbdab8c0477da9419a4
Luan <luanrodriguesdev@gmail.com>
2022-07-13 20:24:28 -0300

    iniciado projeto mestre dos dados

---
+++ commands/dado/dice_gifs.py
@@ -0,0 +1,23 @@
+dice_gifs = {
+    'critical': [
+        'https://tenor.com/view/dice-wind-mill-dice-dancer-dancing-cool-dice-chris-timmons-gif-13865860',
+        'https://tenor.com/view/dungeons-and-dragons-paradise-pd-dnd-20sided-dice-roll-gif-16420028',
+        'https://tenor.com/view/criticalrole-critrole-sam-riegal-mario-bros-jump-gif-20492886',
+        'https://tenor.com/view/la10-la10fc-spin-number20-gif-13576962',
+        'https://tenor.com/view/parrot-dance-celebrate-rave-edm-gif-5362007',
+        'https://tenor.com/view/celebrate-happy-excited-weekend-vibe-gif-5292331',
+        'https://tenor.com/view/happy-happy-dance-seinfeld-yeah-cheering-gif-13573922',
+        'https://tenor.com/view/funny-dogs-me-so-happy-smile-dog-gif-13164685',
+        'https://tenor.com/view/baby-yoda-baby-yoda-dancing-baby-yoda-dance-dance-dance-moves-gif-22982736'
+    ],
+    'critical_error':[
+        'https://tenor.com/view/dog-cachorro-tenso-tense-suspicious-gif-23205154',
+        'https://tenor.com/view/crying-black-guy-meme50fps-interpolated-interpolated-crying-black-guy-crying-crying-black-guy-gif-23747218',
+        'https://tenor.com/view/ok-crying-sad-emotional-sentimental-gif-20646670',
+        'https://tenor.com/view/sad-walk-away-tiger-gif-13828540',
+        'https://tenor.com/view/sad-cat-sunakook-tired-exhausted-gif-24687868',
+        'https://tenor.com/view/lenakos-dog-gif-23957794',
+        'https://tenor.com/view/sad-cry-cat-kitty-tears-gif-24106422',
+        'https://tenor.com/view/meh-reaction-nope-hjrpg-rpg-gif-13900469'
+    ]
+}
